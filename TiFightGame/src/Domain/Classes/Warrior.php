bc30d3f26c92f7f15ea94004d1204ccb386cba5f
Anthony <laforgeanthony85@gmail.com>
2022-11-23 15:51:48 +0100

    Fixes Wizard/Warrior

--- TiFightGame/src/Domain/Classes/Warrior.php
+++ TiFightGame/src/Domain/Classes/Warrior.php
@@ -2,15 +2,14 @@
 require_once('/laragon/www/TiFightGame/src/Domain/Classes/Classes.php');
 class Warrior extends Classes
 {
-    public function __construct(string $name, $weapon = null)
+    public function __construct(string $name, $weapon = "Aucune")
     {
         $this->name = $name;
         $this->health = $this->health + 20;
         $this->damage = $this->damage + 5;
-        if ($weapon != null) {
+        if ($weapon != "Aucune") {
             $this->weapon = $weapon;
-        };
+        }
         $this->class = 'Combattant';
     }
 }
-
--- TiFightGame/src/Domain/Classes/Wizard.php
+++ TiFightGame/src/Domain/Classes/Wizard.php
@@ -2,10 +2,12 @@
 require_once('/laragon/www/TiFightGame/src/Domain/Classes/Classes.php');
 class Wizard extends Classes
 {
-    public function __construct(string $name,$weapon = "Aucune")
-    {   
+    public function __construct(string $name, $weapon = "Aucune")
+    {
         $this->name = $name;
-        $this->weapon = $weapon;
+        if ($weapon != "Aucune") {
+            $this->weapon = $weapon;
+        }
         $this->mana = $this->mana + 50;
         $this->attack_speed = $this->attack_speed - 1;
         $this->class = 'Sorcier';
